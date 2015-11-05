df0f6dbe850677a26392b3b8a0926714d435907d
Ulrik Damm <ulrikdamm@me.com>
2015-11-05 14:01:27 +0100

    v11

--- TablesTest/ViewController.swift
+++ TablesTest/ViewController.swift
@@ -35,7 +35,7 @@ class ViewController : UIViewController {
 		Car(name: "My car", year: 1991, used: true),
 		Car(name: "Mom's car", year: 2010, used: true),
 		Car(name: "Boss' car", year: 2015, used: false),
-	]
+	] { didSet { tableView.tablesDataSource.update(generateSections()) } }
 	
 	func showCar(car : Car) {
 		let vc = UIViewController()
@@ -45,6 +45,12 @@ class ViewController : UIViewController {
 	}
 	
 	override func viewDidLoad() {
+		tableView.tablesDataSource.update(generateSections())
+		
+		navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: Selector("add:"))
+	}
+	
+	func generateSections() -> [Section] {
 		let carCell : ((Int, Car) -> Row) = { i, c in
 			Row("car\(i)", PlainRow(title: c.name, subtitle: String(c.year), action: { [weak self] in self?.showCar(c) }))
 		}
@@ -55,6 +61,16 @@ class ViewController : UIViewController {
 		let newSection = Section("new_cars", header: "New cars", rows: newCars)
 		let oldSection = Section("old_cars", header: "Used cars", rows: oldCars)
 		
-		tableView.tablesDataSource.update([newSection, oldSection])
+		return [newSection, oldSection]
+	}
+	
+	override func viewWillAppear(animated : Bool) {
+		if let indexPath = tableView.indexPathForSelectedRow {
+			tableView.deselectRowAtIndexPath(indexPath, animated: animated)
+		}
+	}
+	
+	func add(sender : AnyObject?) {
+		cars.append(Car(name: "Unnamed car #\(arc4random_uniform(1000))", year: 2000, used: false))
 	}
 }
