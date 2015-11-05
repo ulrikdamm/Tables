6b66316d3486747276b21ae073434a0f3a850859
Ulrik Damm <ulrikdamm@me.com>
2015-11-05 12:45:19 +0100

    Initial Commit

---
+++ Tables/Tables.h
@@ -0,0 +1,19 @@
+//
+//  Tables.h
+//  Tables
+//
+//  Created by Ulrik Damm on 05/11/2015.
+//  Copyright © 2015 Ufd.dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Tables.
+FOUNDATION_EXPORT double TablesVersionNumber;
+
+//! Project version string for Tables.
+FOUNDATION_EXPORT const unsigned char TablesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tables/PublicHeader.h>
+
+
